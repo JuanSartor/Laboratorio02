b87b3a4f26885a93bf1ccf0b5e7f4dc05d4d54c3
Pablo Paletto <ppaletto@frsf.utn.edu.ar>
2018-09-21 18:07:49 -0300

    Se implemento listView en listaPedidos falta el funcionalidad boton

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/lista_prod.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/lista_prod.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,7 @@ public class lista_prod extends AppCompatActivity {
     /*Se definen las variables a utilizar*/
     private Spinner spinnerCategoria;
     private ArrayAdapter<Categoria> adapterCategoria;
-    private ArrayAdapter<Producto> adapterProducto;
+    private productoAdapter adapterProducto;
     private ListView listaProductos;
     private ProductoRepository repositorio;
 
@@ -27,6 +28,7 @@ public class lista_prod extends AppCompatActivity {
         setContentView(R.layout.activity_lista_prod);
 
         /*Se inicializan las variables*/
+        final Context context = this;
         repositorio = new ProductoRepository();
         spinnerCategoria = (Spinner) findViewById(R.id.cmbProductosCategoria);
         adapterCategoria = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,repositorio.getCategorias());
@@ -35,13 +37,25 @@ public class lista_prod extends AppCompatActivity {
         /*Se definen parametros de preferencia para el spinner*/
         adapterCategoria.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCategoria.setAdapter(adapterCategoria);
-        spinnerCategoria.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinnerCategoria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                adapterProducto = new productoAdapter(context, repositorio.buscarPorCategoria((Categoria) (adapterView.getItemAtPosition(i))));
+                listaProductos.setAdapter(adapterProducto);
+                listaProductos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        adapterProducto.setSelectedIndex(i);
+                        adapterProducto.notifyDataSetChanged();
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
     }
-
 
 }
---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/productoAdapter.java
@@ -0,0 +1,53 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+
+import java.util.List;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
+
+public class productoAdapter extends ArrayAdapter<Producto> {
+    private Context ctx;
+    private int mSelectedIndex = -1;
+
+    public productoAdapter(Context act, List<Producto> productos){
+        super(act,0,productos);
+        this.ctx=act;
+    }
+
+    public void setSelectedIndex(int position){
+        this.mSelectedIndex = position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        /*INFLATER*/
+        LayoutInflater inflater = LayoutInflater.from(this.ctx);
+        View fila = convertView;
+        if (fila == null){
+            fila = inflater.inflate(R.layout.product_layout,parent,false);
+        }
+        /*VIEWHOLDER*/
+        productoViewHolder holder = (productoViewHolder) fila.getTag();
+        if (holder == null){
+            holder = new productoViewHolder(fila);
+            fila.setTag(holder);
+        }
+
+        Producto prod = (Producto) super.getItem(position);
+        holder.nombre.setText(prod.getNombre());
+        holder.precio.setText("$("+prod.getPrecio().toString()+")");
+        if (mSelectedIndex == position){
+            holder.seleccion.setChecked(true);
+        }else {
+            holder.seleccion.setChecked(false);
+        }
+
+        return fila;
+    }
+}

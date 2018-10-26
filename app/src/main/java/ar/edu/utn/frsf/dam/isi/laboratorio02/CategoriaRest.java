b0263018195fb6e11c5669ab966969a84a01be70
David Harispe <david_h.94@hotmail.com>
2018-10-26 13:04:53 -0300

    se inicio la consulta de categorias con el servidor

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CategoriaRest.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CategoriaRest.java
@@ -2,8 +2,10 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
@@ -15,11 +17,12 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
 
 public class CategoriaRest {
-        // realiza el POST de una categoría al servidor REST
 
         public void crearCategoria(Categoria c) {
             try{
@@ -27,12 +30,9 @@ public class CategoriaRest {
             DataOutputStream printout;
             InputStream in;
 
-            //Crear el objeto json que representa una categoria
             JSONObject categoriaJson = new JSONObject();
             categoriaJson.put("nombre", c.getNombre());
 
-
-            //Abrir una conexión al servidor para enviar el POST
             URL url = new URL("http://10.0.2.2:5000/categorias");
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setChunkedStreamingMode(0);
@@ -40,20 +40,17 @@ public class CategoriaRest {
             urlConnection.setDoOutput(true);
             urlConnection.setRequestMethod("POST");
 
-            //Obtener el outputStream para escribir el JSON
             printout = new DataOutputStream(urlConnection.getOutputStream());
             String str = categoriaJson.toString();
             byte[] jsonData = str.getBytes("UTF-8");
             printout.write(jsonData);
             printout.flush();
 
-            //Leer la respuesta
             in = new BufferedInputStream(urlConnection.getInputStream());
             InputStreamReader isw = new InputStreamReader(in);
             StringBuilder sb = new StringBuilder();
             int data = isw.read();
 
-            //Analizar el codigo de lar respuesta
             if (urlConnection.getResponseCode() == 200 ||
                     urlConnection.getResponseCode() == 201) {
                 while (data != -1) {
@@ -61,13 +58,10 @@ public class CategoriaRest {
                     sb.append(current);
                     data = isw.read();
                 }
-                //analizar los datos recibidos
                 Log.d("LAB_04", sb.toString());
             } else {
                 Log.d("Error", "se produjo un error en el proceso!");
             }
-                // caputurar todas las excepciones y en el bloque finally
-                // cerrar todos los streams y HTTPUrlCOnnection
                 if(printout!=null)  printout.close();
                 if(in!=null) in.close();
                 if(urlConnection !=null)urlConnection.disconnect();
@@ -75,4 +69,59 @@ public class CategoriaRest {
                 Log.e("Error", e.getMessage());
             }
     }
+
+    // definir el método
+    public List<Categoria> listarTodas(){
+        // inicializar variables
+        List<Categoria> resultado = new ArrayList<>();
+        HttpURLConnection urlConnection = null;
+        InputStream in =null;
+
+        try{
+            URL url = new URL("http://10.0.2.2:5000/categorias");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Accept-Type","application/json");
+            urlConnection.setRequestMethod("GET");
+
+            // Leer la respuesta
+            in = new BufferedInputStream(urlConnection.getInputStream());
+            InputStreamReader isw = new InputStreamReader(in);
+            StringBuilder sb = new StringBuilder();
+            int data = isw.read();
+
+            // verificar el codigo de respuesta
+            if( urlConnection.getResponseCode() ==200 ||
+                    urlConnection.getResponseCode()==201){
+                while (data != -1) {
+                    char current = (char) data;
+                    sb.append(current);
+                    data = isw.read();
+                }
+                // ver datos recibidos
+                Log.d("LAB_04",sb.toString());
+                // Transformar respuesta a JSON
+                JSONTokener tokener = new JSONTokener(sb.toString());
+                JSONArray listaCategorias = (JSONArray) tokener.nextValue();
+
+                // iterar todas las entradas del arreglo
+                for(...){
+                    Categoria cat = new Categoria();
+                    // analizar cada element del JSONArray
+                    //armar una instancia de categoría y agregarla a la lista
+                    resultado.add(cat);
+                }
+            }else{
+                // lanzar excepcion o mostrar mensaje de error
+                // que no se pudo ejecutar la operacion
+            }
+            //NO OLVIDAR CERRAR inputStream y conexion
+            if(in!=null) in.close();
+            if(urlConnection !=null)urlConnection.disconnect();
+        }
+        catch (Exception e) {
+            Log.e("Error", e.getMessage());
+        }
+
+        return resultado;
+        }
 }
